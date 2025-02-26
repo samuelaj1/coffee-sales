73d555b3beb413ea0d356dc61001a5ca75649252
NanaT <samuelagyekumhene@gmail.com>
2025-02-26 15:07:51 +0000

    fix: localise bootstrap and vue js scripts

--- resources/views/base/pageBody.blade.php
+++ resources/views/base/pageBody.blade.php
@@ -8,10 +8,9 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <!-- Bootstrap for styling -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="{{asset('css/bootstrap.min.css')}}" rel="stylesheet">
 
     <!-- Vue 2 -->
-    {{--    <script src="https://cdn.jsdelivr.net/npm/vue@2.6.14/dist/vue.js"></script>--}}
     <script src="{{asset('/js/vue.js')}}"></script>
     {{--    axios--}}
     <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
