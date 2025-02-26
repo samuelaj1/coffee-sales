2676dba8a93324989b0c132bafb5b3f743e2acaa
NanaT <samuelagyekumhene@gmail.com>
2025-02-26 13:49:30 +0000

    feat: product migrations

---
+++ app/Models/Product.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'description',
+        'primary_product',
+        'profit_margin'
+    ];
+
+    protected $casts = [
+        'primary_product' => 'boolean',
+        'profit_margin' => 'float',
+    ];
+}
